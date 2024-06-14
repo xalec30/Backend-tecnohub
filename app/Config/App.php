f6e1cecca075df90f68806ddcfb963a4e0beda0e
Alex Rodriguez <xalecr30@gmail.com>
2024-06-14 00:59:19 -0400

    modificacion

--- app/Config/App.php
+++ app/Config/App.php
@@ -16,7 +16,7 @@ class App extends BaseConfig
      *
      * E.g., http://example.com/
      */
-    public string $baseURL = 'https://backend-tecnohub-production.up.railway.app/';
+    public string $baseURL = 'https://localhost:8080/';
 
     /**
      * Allowed Hostnames in the Site URL other than the hostname in the baseURL.
