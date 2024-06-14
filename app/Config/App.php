9710a70a992ed5748b965e180324ee0984cb0e22
Alex Rodriguez <xalecr30@gmail.com>
2024-06-14 08:49:30 -0400

    modificado bae url

--- app/Config/App.php
+++ app/Config/App.php
@@ -16,7 +16,7 @@ class App extends BaseConfig
      *
      * E.g., http://example.com/
      */
-    public string $baseURL = 'https://backend-proyecto.fly.dev';
+    public string $baseURL = 'http://50.17.23.95/';
 
     /**
      * Allowed Hostnames in the Site URL other than the hostname in the baseURL.
