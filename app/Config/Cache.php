135fad142735891ea1973b446ad906c04980c382
Alex Rodriguez <xalecr30@gmail.com>
2024-06-15 01:23:30 -0400

    caching

--- app/Config/Cache.php
+++ app/Config/Cache.php
@@ -45,7 +45,7 @@ class Cache extends BaseConfig
      * @deprecated Use the driver-specific variant under $file
      */
     //public string $storePath = WRITEPATH . 'cache/';
-    public string $storePath = '';
+    public string $storePath = '/tmp/';
 
     /**
      * --------------------------------------------------------------------------
@@ -94,7 +94,7 @@ class Cache extends BaseConfig
      */
     public array $file = [
         //'storePath' => WRITEPATH . 'cache/',
-        'storePath' => '',
+        'storePath' => '/tmp/',
         'mode'      => 0640,
     ];
 
