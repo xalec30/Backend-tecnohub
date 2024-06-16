0f596c65a02291d241a55519b88c38609ffdeed6
Alex Rodriguez <xalecr30@gmail.com>
2024-06-16 11:49:19 -0400

    modificacion de rutas

--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -12,6 +12,7 @@ $routes->resource('api/v1/categories',['controller' => 'Api\V1\Categories']);
 $routes->resource('api/v1/tags',['controller' => 'Api\V1\Tags']);
 $routes->resource('api/v1/roles',['controller' => 'Api\V1\Roles']);
 $routes->resource('api/v1/comments',['controller' => 'Api\V1\Comments']);
+$routes->resource('api/v1/comments',['controller' => 'Api\V1\Comments']);
 
 $routes->resource('api/v1/auth',['controller' => 'Api\V1\Auth']);
 
